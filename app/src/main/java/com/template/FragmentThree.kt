7ab472e391ee93483ed414e922ed920de59b6d04
borshevskiy <sk8er1001@mail.ru>
2022-06-02 00:55:34 +0600

    some code

---
+++ app/src/main/java/com/template/FragmentThree.kt
@@ -0,0 +1,28 @@
+package com.template
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.template.databinding.FragmentThreeBinding
+
+class FragmentThree : Fragment() {
+
+    private var _binding: FragmentThreeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentThreeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
